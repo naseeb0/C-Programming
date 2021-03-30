29337ea706b7e7c5509725648ebef3d508eb5b3c
Naseb <nasheeb.dangi@gmail.com>
2021-03-30 16:53:17 +0545

    Hello

---
+++ Assignments/Simple_Function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void multiply(int x,int y ){
+    int result = x*y;
+    printf("Multiplication = %d",result);
+}
+
+
+int main(){
+    multiply(3, 4);
+    multiply(5, 10);
+    multiply(6, 0);
+    
+                
+}
